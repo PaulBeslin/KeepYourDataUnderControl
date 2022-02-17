ed9163a71e6c339acb046843a782560ec41ede73
Paul Beslin <paul.beslin@gmail.com>
2022-02-17 16:17:58 +0100

    Affichage fonctionnel pour les textes

--- extension/src/manage.js
+++ extension/src/manage.js
@@ -6,6 +6,18 @@ const DB_URL = "http://localhost:5001/";
 const DELETE_URL = DB_URL + "remove/";
 const GET_ALL_URL = DB_URL + "all";
 
+/**
+ * Structure of a userdata retrieved from the database.
+ * - data: URL to the userdata in database, ex.: "http://localhost:5001/query/resource/31d81771-d9fc-4922-87bf-5488fbf2f495"
+ * - id: UUID of the data, ex.: "31d81771-d9fc-4922-87bf-5488fbf2f495"
+ * - type: type of userdata, either 'image' or 'text'.
+ * @typedef {Object} DataStruct
+ *  @property {string} data - URL to the userdata in database, 
+ *                            ex.: "http://localhost:5001/query/resource/31d81771-d9fc-4922-87bf-5488fbf2f495"
+ *  @property {string} id - UUID of the userdata, 
+ *                          ex.: "31d81771-d9fc-4922-87bf-5488fbf2f495"
+ *  @property {string} type - type of userdata, either 'image' or 'text'.
+ */
 
 /**
  * Deletes the selected data from the database,
@@ -26,28 +38,42 @@ function removeData(dataId, htmlFrame){
         data: body,
         dataType: "json"
       })
-        .then(_ => {console.log("SUCCESS")});
+        .then(_ => {console.log(`Successfully deleted data with ID: ${dataId}`)});
 }
 
 /**
  * Data part of the data container (see createDataContainer)
  * 
- * @param {string} imgSrc The database URL from where to get the data.
+ * @param {DataStruct} data The database URL from where to get the data.
  * @returns {HTMLDivElement} A div containing the data to display.
  */
-function createDataFrame(imgSrc){
-    //imgSrc = exampleImgSrc //TEMP
-
+function createDataFrame(data){
     //Data div
     let dataDiv = document.createElement('div');
     dataDiv.className = "data";
 
-    //Image data
-    let img = document.createElement('img');
-    img.className = "limit-size";
-    img.src = imgSrc;
+    let dataElem;
+    switch(data.type){
+        //Image data
+        case "image":
+            dataElem = document.createElement('img');
+            dataElem.className = "limit-size";
+            dataElem.src = data.data;
+            break;
+        case "text":
+            dataElem = document.createElement('p');
+            $.ajax({
+                type: "GET",
+                url: data.data
+            })
+                .then(text => {dataElem.innerText = text});
+            break;
+        default:
+            console.error(`Could not display data type: ${data.type}`);
+            dataElem = document.createElement('div');
+    }
 
-    dataDiv.appendChild(img);
+    dataDiv.appendChild(dataElem);
 
     return dataDiv;
 }
@@ -81,24 +107,24 @@ function createMenuButton(dataId, container){
 /**
  * Creates an HTML node to display the provided data.
  * 
- * @param {number} dataId The ID in database for the data to display.
- *                        Used when interacting with the data.
- * @param {string} imgSrc The database URL from where to get the data.
+ * @param {DataStruct} data Information about the data to display. 'data.id' allows to interact with the data,
+ *                        and 'data.data' to get the data itself.
  * @returns {HTMLDivElement} An HTML node representing the data and options to interact with it.
  */
-function createDataContainer(dataId, imgSrc){
+function createDataContainer(data){
     //Data+button frame
     let container = document.createElement('div');
     container.className = "data-container";
 
     //Data frame
-    container.appendChild(createDataFrame(imgSrc));
+    container.appendChild(createDataFrame(data));
 
     //Button frame
-    container.appendChild(createMenuButton(dataId, container));
+    container.appendChild(createMenuButton(data.id, container));
 
     return container;
 }
+
 
 
 
@@ -110,7 +136,7 @@ $.ajax({
 })
     .then(dataList => {
         dataList.forEach(data => {
-            document.body.appendChild(createDataContainer(data.id, data.data));
+            document.body.appendChild(createDataContainer(data));
         })
     });
 
